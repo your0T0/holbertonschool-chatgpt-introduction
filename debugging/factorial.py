b05e7995a55d9eef55275a4dac3373fea49ec72f
your0T0 <yara241020@gmail.com>
2026-01-11 09:52:29 +0000

    fixing task 1

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,12 +1,12 @@
-â#!/usr/bin/python3
-âimport sys
+#!/usr/bin/python3
+import sys
 
-âdef factorial(n):
-â    result = 1
-â    while n > 1:
-â        result *= n
-â        n -= 1
-â    return result
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
 
-âf = factorial(int(sys.argv[1]))
-âprint(f)
+f = factorial(int(sys.argv[1]))
+print(f)
